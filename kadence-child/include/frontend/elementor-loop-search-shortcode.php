8d064cb299f39d8f9862f5a8a22bdd2886a17fae
Kazi Sadib Reza <kazisadibreza@gmail.com>
2025-11-29 09:44:58 +0600

    Fix location search - add ACF subfield detection for location_ fields

--- kadence-child/include/frontend/elementor-loop-search-shortcode.php
+++ kadence-child/include/frontend/elementor-loop-search-shortcode.php
@@ -26,6 +26,10 @@ class Elementor_Loop_Search_Shortcode {
         
         // Hook into Elementor query for query ID 6969 (add more as needed)
         add_action('elementor/query/6969', array($this, 'filter_elementor_query'), 10, 2);
+        
+        // AJAX handler
+        add_action('wp_ajax_elementor_loop_search', array($this, 'ajax_search'));
+        add_action('wp_ajax_nopriv_elementor_loop_search', array($this, 'ajax_search'));
     }
     
     /**
@@ -43,7 +47,7 @@ class Elementor_Loop_Search_Shortcode {
             'elementor-loop-search',
             get_stylesheet_directory_uri() . '/assets/js/elementor-loop-search.js',
             array('jquery'),
-            '1.2.0',
+            '1.3.0',
             true
         );
         
@@ -115,20 +119,93 @@ class Elementor_Loop_Search_Shortcode {
         
         // Build meta query to search in location fields (like property filter)
         if (!empty($this->location_meta_key)) {
+            // Get all possible location subfields from ACF
+            $location_subfields = $this->get_location_subfields($this->location_meta_key);
+            
             $meta_query = array(
                 'relation' => 'OR',
-                // Search in location meta field
-                array(
-                    'key' => $this->location_meta_key,
+            );
+            
+            // Search in each location subfield
+            foreach ($location_subfields as $subfield) {
+                $meta_query[] = array(
+                    'key' => $subfield,
                     'value' => $search_keyword,
                     'compare' => 'LIKE'
-                ),
-                // Add more location-related fields if needed
-                // Similar to property filter searching in street, city, region, country
-            );
+                );
+            }
             
             $query->set('meta_query', $meta_query);
         }
+    }
+    
+    /**
+     * Get all location subfields from ACF field structure
+     * Similar to how property filter gets field structure
+     */
+    private function get_location_subfields($field_key) {
+        $subfields = array($field_key); // Always include the main field
+        
+        // Method 1: Try to get field structure from ACF
+        if (function_exists('acf_get_field')) {
+            $field = acf_get_field($field_key);
+            if ($field && isset($field['sub_fields']) && !empty($field['sub_fields'])) {
+                foreach ($field['sub_fields'] as $subfield) {
+                    $subfields[] = $field_key . '_' . $subfield['name'];
+                }
+                return $subfields;
+            }
+        }
+        
+        // Method 2: Try get_field_object with a sample post
+        if (function_exists('get_field_object')) {
+            $sample_post = get_posts(array(
+                'post_type' => 'any',
+                'posts_per_page' => 1,
+                'post_status' => 'publish'
+            ));
+            
+            if (!empty($sample_post)) {
+                $field = get_field_object($field_key, $sample_post[0]->ID);
+                if ($field && isset($field['sub_fields']) && !empty($field['sub_fields'])) {
+                    foreach ($field['sub_fields'] as $subfield) {
+                        $subfields[] = $field_key . '_' . $subfield['name'];
+                    }
+                    return $subfields;
+                }
+            }
+        }
+        
+        // Method 3: Common location subfield patterns (fallback)
+        // These are standard ACF location field subfields
+        $common_subfields = array(
+            $field_key . '_street_number',
+            $field_key . '_street_name',
+            $field_key . '_city',
+            $field_key . '_state',
+            $field_key . '_post_code',
+            $field_key . '_country',
+            $field_key . '_address', // Full address
+        );
+        
+        return array_merge($subfields, $common_subfields);
+    }
+    
+    /**
+     * AJAX handler for search (optional - currently using redirect)
+     */
+    public function ajax_search() {
+        check_ajax_referer('elementor_loop_search_nonce', 'nonce');
+        
+        $search_keyword = isset($_POST['search_keyword']) ? sanitize_text_field($_POST['search_keyword']) : '';
+        $query_id = isset($_POST['query_id']) ? sanitize_text_field($_POST['query_id']) : '';
+        
+        wp_send_json_success(array(
+            'redirect_url' => add_query_arg(array(
+                'search_keyword' => $search_keyword,
+                'query_id' => $query_id
+            ), home_url($_SERVER['REQUEST_URI']))
+        ));
     }
     
 }
